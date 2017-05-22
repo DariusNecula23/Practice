09c99980f742fe57de9123ec9e00038109525414
DariusNecula23 <neculadarius23@gmail.com>
2017-05-22 17:15:40 +0300

    interfata grafica

---
+++ Practice02/app/src/main/java/practice02/eim/systems/cs/pub/ro/practice02/PracticeMainActivity.java
@@ -0,0 +1,13 @@
+package practice02.eim.systems.cs.pub.ro.practice02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PracticeMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practice_main);
+    }
+}
